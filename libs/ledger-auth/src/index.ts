2d698dd6585459044cff005fbebd9a07cbd4af1c
Theophile Sandoz <theophile.sandoz@ledger.fr>
2026-05-15 18:16:44 +0200

    feat(ledger-auth): add jwt authentication flow

---
+++ libs/ledger-auth/src/index.ts
@@ -0,0 +1,13 @@
+export { authenticate } from "./authenticate";
+export type {
+  AuthParams,
+  AuthService,
+  AuthConfig,
+  Signer,
+  ChallengeRequest,
+  ChallengeResponse,
+  AuthorizeRequest,
+  AuthorizeResponse,
+  TokenRequest,
+  TokenResponse,
+} from "./types";
