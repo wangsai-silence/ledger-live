bc5c4ae794671d6a774a1e51a6d439ce6b4090d1
qperrot <quentin.perrot-minnot@ledger.fr>
2026-05-15 15:01:44 +0200

    fix: use brackets for bignumber import on evm

--- libs/coin-modules/coin-evm/src/staking/logic.test.ts
+++ libs/coin-modules/coin-evm/src/staking/logic.test.ts
@@ -1,4 +1,4 @@
-import BigNumber from "bignumber.js";
+import { BigNumber } from "bignumber.js";
 import type { StakingAccount } from "../types";
 import { getMaxEstimatedBalance } from "./logic";
 
